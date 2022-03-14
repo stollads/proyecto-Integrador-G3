10e22bb7f56872823df8ec29bb89c9134c34f296
Santi Puntieri <santipuntieri@gmail.com>
2022-03-14 20:58:35 -0300

    actualizando

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 // ************ Controller Require ************
-const mainController = require('../controllers/mainControllers');
+/*const mainController = require('../controllers/mainControllers');
 
 router.get('/', mainControllers.index); 
-router.get('/search', mainControllers.search); 
+router.get('/search', mainControllers.search); */
 
 module.exports = router;
 
